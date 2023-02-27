5814805c8a3d38c41049feab73e1e6570e07115f
Dmitry Nichiporenko <excelsiorious@gmail.com>
2023-02-27 20:15:57 +0300

    Add Construct Quad Tree algorithm

---
+++ src/test/java/com/nichiporenko/algorithms/arrays2d/medium/ConstructQuadTreeTest.java
@@ -0,0 +1,14 @@
+package com.nichiporenko.algorithms.arrays2d.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructQuadTreeTest {
+
+    @Test
+    void construct() {
+        // TODO: add test
+    }
+}
