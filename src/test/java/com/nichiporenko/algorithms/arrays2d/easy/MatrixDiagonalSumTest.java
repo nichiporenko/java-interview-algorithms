d01adfc187bf2725cb1c3177fce7f5c25736def1
Dmitry Nichiporenko <excelsiorious@gmail.com>
2023-05-09 17:25:26 +0300

    Add Matrix Diagonal Sum algorithm

---
+++ src/test/java/com/nichiporenko/algorithms/arrays2d/easy/MatrixDiagonalSumTest.java
@@ -0,0 +1,31 @@
+package com.nichiporenko.algorithms.arrays2d.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MatrixDiagonalSumTest {
+    private final int[][] input1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+    private final int[][] input2 = {
+            {1, 1, 1, 1},
+            {1, 1, 1, 1},
+            {1, 1, 1, 1},
+            {1, 1, 1, 1}
+    };
+    private final int[][] input3 = {
+            {5}
+    };
+
+    @Test
+    void floodFill() {
+        assertThat(MatrixDiagonalSum.diagonalSum(input1)).isEqualTo(25);
+        assertThat(MatrixDiagonalSum.diagonalSum(input2)).isEqualTo(8);
+        assertThat(MatrixDiagonalSum.diagonalSum(input3)).isEqualTo(5);
+    }
+
+
+}
